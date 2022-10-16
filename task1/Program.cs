107fa7d662852bd5fbe775872944898e44932d76
Илья Корытов <t.b.o.l@mail.ru>
2022-10-16 18:03:23 +0400

    Добавьте файлы проекта.

---
+++ task1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+class MainClass
+{
+    static void Main(string[] args)
+    {
+        (string Name, string[] Dishes) User;
+        
+
+
+        Console.WriteLine("Введите имя");
+        User.Name = Console.ReadLine();
+
+        User.Dishes = new string[5];
+        for (int i = 0; i < User.Dishes.Length; i++)
+        {
+            Console.WriteLine("Введите ваше любимое блюдо {0}", i + 1);
+            User.Dishes[i] = Console.ReadLine(); 
+        }
+        Console.ReadKey();    
+        
+    
+
+    }
+}
